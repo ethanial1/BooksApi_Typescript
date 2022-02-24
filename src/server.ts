b13bfc6a584750be394cf98427be241956205a24
ethanial1 <camiloeiz00@gmail.com>
2022-02-24 17:05:59 -0600

    inicialización del servidor con typescript

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from 'morgan'
+import helmet from "helmet";
+
+class Server {
+    public app: express.Application;
+
+    constructor() {
+        this.app = express()
+        this.config()
+    }
+
+    config() {
+        this.app.set('port', process.env.PORT || 3001)
+        // middlewares
+        this.app.use(morgan('dev'))
+        this.app.use(helmet())
+    }
+
+    routes() {
+
+    }
+
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log("Server listening on port",this.app.get('port'))
+        })
+    }
+}
+
+
+const server = new Server()
+server.start();
